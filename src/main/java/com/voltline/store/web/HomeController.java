46ed808dc4c730ce92323c749a53a05d77ffb3df
Nick Kuhn <95254386+nkuhn-vmw@users.noreply.github.com>
2026-06-16 15:09:40 -0400

    Add storefront homepage (HomeController + home.html + CSS)

---
+++ src/main/java/com/voltline/store/web/HomeController.java
@@ -0,0 +1,21 @@
+package com.voltline.store.web;
+
+import com.voltline.store.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "home";
+    }
+}
